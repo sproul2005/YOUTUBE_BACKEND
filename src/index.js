0c74fa0fc942c93eee9f258cb2fda20b110e3551
Delta Student <satyaprakashroul324>
2026-02-11 16:01:34 +0530

    Updated API logic

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
 //require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 
-
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./config/index.js";
 
-
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
-connectDB();
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+  });
 
 /*
 import express from "express";
