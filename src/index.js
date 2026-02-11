69faaf11923f871327e4767949d6d54458cba68e
Delta Student <satyaprakashroul324>
2026-02-11 10:22:14 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,43 @@
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./config/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERR:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("ERROR:", error);
+    throw err;
+  }
+})();
+*/
