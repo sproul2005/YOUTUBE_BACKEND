43a5d4484b16b31f5ca551e9af75a7c26b3b7048
Delta Student <satyaprakashroul324>
2026-02-20 16:21:19 +0530

    add models

---
+++ src/models/comment.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamp: true }
+);
+
+commentSchema.plugin(mongooseAggregatePaginate);
+
+export const Comment = mongoose.model("Comment", commentSchema);
